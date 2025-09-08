de0d37ee718332cada6a5888ad93f3008d6eb969
Adarsha Adhikari <adarsha.ncer@gmail.com>
2025-09-08 17:44:42 -0500

    Submission - Adarsha Adhikari

---
+++ Adarsha/hw2/1.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+x = np.linspace(0, 3, 500)
+y = x - (3/2) * x**2 + 0.5 * x**3
+
+# Plot
+plt.figure(figsize=(8,6))
+plt.plot(x, y)
+plt.xlabel(r'$-\frac{w}{z}$')
+plt.ylabel(r'$-\frac{W}{EA}\left(\frac{L}{z}\right)^3$')
+plt.title('Problem 1: Normalized force vs (-w/z)')
+plt.legend()
+plt.grid(True)
+plt.show()
